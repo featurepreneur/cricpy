537e8b8f3462cd11d351621931f7fe9db362640c
Talha-Altair <cooltalha27@gmail.com>
2022-02-21 20:17:30 +0530

    Updates for 2022-02-21 20:17:30

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ requirements = [requirement for requirement in open('requirements.txt')]
 
 setuptools.setup(
     name="cricpy2",                     # This is the name of the package
-    version="0.0.7",                        # The initial release version
+    version="0.0.9",                        # The initial release version
     author="Featureprenuer",                     # Full name of the author
     description="A Cricket Game",
     long_description=long_description,      # Long description read from the the readme file
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    packages=setuptools.find_packages(include=["cricpy2", "cricpy2.*"]),    # List of all python modules to be installed
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -21,6 +21,6 @@ setuptools.setup(
     ],                                      # Information to filter the project on PyPi website
     python_requires='>=3.6',                # Minimum version requirement of the package
     py_modules=["cricpy2"],             # Name of the python package
-    package_dir={'':'cricpy2'},    # Directory of the source code of the package
+    package_dir={'':'cricpy2'},# Directory of the source code of the package
     install_requires=requirements                  # Install other dependencies if any
 )
